5513a28c53544728cac93c957c95e5f9c14a1b27
phiby pinky <phiby8888@gmail.com>
2022-03-28 16:55:39 -0700

    This is an asignment

---
+++ assets/js/Temperature.js
@@ -0,0 +1,11 @@
+
+let temperature=22;
+if(temperature>=17 && temperature<=30){
+    console.log('Your temperature qualifies to be called normal!')
+}else if (temperature>=37 && temperature<=40){
+    console.log('Your temperature will be described as above normal')
+}else if (temperature>=40){
+    console.log('Please check into hospital ')
+}else{
+    console.log('temperature is invalid')
+}
